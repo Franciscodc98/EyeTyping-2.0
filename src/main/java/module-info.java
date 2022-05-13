342fb7af62c102b10d66671e52495997754884ee
Francisco Cardoso <60990445+Franciscodc98@users.noreply.github.com>
2022-05-13 01:34:22 +0100

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.eyetyping.eyetyping2 {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.annotation;
-    requires opencsv;
+    requires com.opencsv;
 
     opens com.eyetyping.eyetyping2 to javafx.fxml;
     exports com.eyetyping.eyetyping2;
