69e71a632bd6defa782df23bbc1c1f012009d23e
Francisco Cardoso <60990445+Franciscodc98@users.noreply.github.com>
2022-05-23 13:23:02 +0100

    update

--- src/main/java/com/eyetyping/eyetyping2/utils/GlobalVariables.java
+++ src/main/java/com/eyetyping/eyetyping2/utils/GlobalVariables.java
@@ -4,6 +4,7 @@ public class GlobalVariables {
 
     private GlobalVariables(){}
 
+
     public static final String USER_ID_PATH = "src/main/resources/UserId.txt";
 
     public static final String PHRASES_PATH = "src/main/resources/TestingPhrasesDataset.txt";
@@ -13,6 +14,7 @@ public class GlobalVariables {
     public static final String ALPHABET_LETTERS_EXTENDED = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!?.,";
 
     public static final String WORDS_20K_PATH = "src/main/resources/Top20kEnglishWords";
+    public static final String WORDS_333K_PATH = "src/main/resources/333kWords.txt";
 
     public static final String CONFIRM_SOUND_PATH = "src/main/resources/sounds/beep.wav";
 
